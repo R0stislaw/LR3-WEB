2789effd5817131471f88664796087d2a5e671ae
R0stislaw <115527575+R0stislaw@users.noreply.github.com>
2023-10-21 22:44:44 +0300

    Add files via upload

--- LR3/scripts/script4.js
+++ LR3/scripts/script4.js
@@ -1,18 +1,24 @@
 var myButton = document.getElementById("myButton");
 
 function miniGame() {
-    const numberInput = document.getElementById("numberInput");
+  const numberInput = document.getElementById("numberInput");
+  if (numberInput.value >= 1) {
     var randomInteger = Math.floor(Math.random() * 11) - 5;
     console.log(randomInteger);
     if (randomInteger <= 0) {
-        alert("Ваша ставка не зіграла, ви програли");
+      alert("Ваша ставка не зіграла, ви програли");
     } else if (randomInteger <= 5 || randomInteger > 0) {
-        alert("Ваша ставка зіграла, ви виграли - " + numberInput.value * randomInteger);
+      alert(
+        "Ваша ставка зіграла, ви виграли " + numberInput.value * randomInteger
+      );
     }
     myButton.disabled = false;
+  }else{
+    alert("Некоректна сума ставки")
+    myButton.disabled = false;
+  }
 }
-
-myButton.addEventListener('click', function() {
-    myButton.disabled = true;
-    setTimeout(miniGame, 2000);
+myButton.addEventListener("click", function () {
+  myButton.disabled = true;
+  setTimeout(miniGame, 2000);
 });
